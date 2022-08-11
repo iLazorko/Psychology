2b748af7205e9e34ce11cbd8f593ad7050b7bcfe
Inna Lazorko <inna-h@i.ua>
2022-08-11 16:18:38 +0300

    add

---
+++ src/js/spesialists-slider.js
@@ -0,0 +1,94 @@
+import Swiper from 'swiper/swiper-bundle';
+
+const swiper = new Swiper('.mySwiperSpesialists', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyOnViewport: true,
+    padeUpDown: true,
+  },
+
+  autoplay: {
+    delay: 5000,
+    stopOnLastSlide: false,
+    disableOnIteration: false,
+  },
+  loop: true,
+  initialSlide: 1,
+  breakpoints: {
+    370: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 50,
+    },
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      spaceBetween: 50,
+    },
+    1000: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 50,
+    },
+    1469: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 19,
+    },
+  },
+});
+
+// if (outerWidth < 768) {
+//   const swiper = new Swiper('.mySwiperSpesialists', {
+//     effect: 'cards',
+//     grabCursor: true,
+//     keyboard: {
+//       enabled: true,
+//       pageUpDown: true,
+//     },
+//     breakpoints: {
+//       slidesPerView: 0.1,
+//       slidesPerGroup: 1,
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//   });
+// } else {
+//   const swiper = new Swiper('.mySwiperSpesialists', {
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//     keyboard: {
+//       enabled: true,
+//       onlyOnViewport: true,
+//       padeUpDown: true,
+//     },
+//     spaceBetween: 19,
+//     autoplay: {
+//       delay: 5000,
+//       stopOnLastSlide: false,
+//       disableOnIteration: false,
+//     },
+//     speed: 1000,
+//     breakpoints: {
+//       768: {
+//         slidesPerView: 3,
+//         slidesPerGroup: 3,
+//         spaceBetween: 30,
+//         loop: true,
+//       },
+//       1469: {
+//         slidesPerView: 3,
+//         slidesPerGroup: 3,
+//         spaceBetween: 19,
+//       },
+//     },
+//   });
+// }
